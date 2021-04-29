4f15ca4b999a2654070d6479f3fb5803441c89bd
leejiwon6315 <leejiwon6315@naver.com>
2021-04-29 19:31:47 +0900

    update 210429

--- meemo-server/server.js
+++ meemo-server/server.js
@@ -214,11 +214,11 @@ app.post("/api/get/todo", (req, res) => {
 const port = process.env.PORT || 5000;
 const notesRouter = require("./routes/notes");
 const foldersRouter = require("./routes/folders");
-// const storageRouter = require("./routes/storage");
+const storageRouter = require("./routes/storage");
 
 app.use("/api/notes", notesRouter);
 app.use("/api/folders", foldersRouter);
-// app.use("/api/s3", storageRouter);
+app.use("/api/s3", storageRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
