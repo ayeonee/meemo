58174c30f08c9fca6bfb22c72f950e01f5b1c12c
leejiwon6315 <leejiwon6315@naver.com>
2021-05-04 17:05:03 +0900

    modified error page

--- meemo-client/src/pages/unknown/index.tsx
+++ meemo-client/src/pages/unknown/index.tsx
@@ -1,9 +1,21 @@
 import style from "./styles/error.module.scss";
+import unknown from "../../img/unknown.svg";
+
 export default function UnkownPage(): JSX.Element {
   return (
     <div className={style.error_page}>
-      <div className={style.error_message_wrapper}>
-        <h1>Oops!</h1>
+      <div className={style.error_box_wrapper}>
+        <h1 className={style.title}>Oooooooops!</h1>
+        <h1 className={style.sub_title}>
+          This page is unknown or does not exit.
+        </h1>
+        <div className={style.section}>
+          <img src={`${unknown}`} alt="block icon" />
+          <div className={style.script}>
+            <p>인터넷이 연결되어있지 않거나, 존재하지 않는 페이지입니다.</p>
+            <p>인터넷 연결상태를 확인해 주세요.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
